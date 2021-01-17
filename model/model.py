f58082ab3ed3024f31efe4e19165e5433b30d29c
v.seliverstov <v.seliverstov@qbsw.sk>
2021-01-17 21:51:04 +0100

    rewrite model class, start training script

--- model/model.py
+++ model/model.py
@@ -1,59 +1,63 @@
 import tensorflow
 from tensorflow import keras
+from tensorflow.keras import activations
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers.convolutional import SeparableConv2D, MaxPooling2D
 from tensorflow.keras.layers.convolutional import MaxPooling2D
 from tensorflow.keras.layers.core import Activation, Flatten, Dropout, Dense
 from tensorflow.keras import backend as K
 
+# Class for building neural net model.
 class CancerNet:
-  @staticmethod
-  def build(width,height,depth,classes):
-    model=Sequential()
-    shape=(height,width,depth)
-    channelDim=-1
+    @staticmethod
+    def build(width,height,depth,classes):
+        same_padding = "same"
+        shape=(height,width,depth)
+        channelDim=-1
 
-    if K.image_data_format()=="channels_first":
-      shape=(depth,height,width)
-      channelDim=1
+        if K.image_data_format()=="channels_first":
+            shape=(depth,height,width)
+            channelDim=1
 
-    model.add(SeparableConv2D(32, (3,3), padding="same",input_shape=shape))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=channelDim))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+        model=Sequential([
+            SeparableConv2D(32, (3,3), padding = same_padding, input_shape = shape),
+            Activation(activations.relu),
+            BatchNormalization(axis = channelDim),
+            MaxPooling2D(pool_size = (2,2)),
+            Dropout(0.25),
 
-    model.add(SeparableConv2D(64, (3,3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=channelDim))
+            SeparableConv2D(64, (3,3), padding = same_padding),
+            Activation(activations.relu),
+            BatchNormalization(axis = channelDim),
 
-    model.add(SeparableConv2D(64, (3,3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=channelDim))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+            SeparableConv2D(64, (3,3), padding = same_padding),
+            Activation(activations.relu),
+            BatchNormalization(axis = channelDim),
+            MaxPooling2D(pool_size = (2,2)),
+            Dropout(0.25),
 
-    model.add(SeparableConv2D(128, (3,3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=channelDim))
+            SeparableConv2D(128, (3,3), padding = same_padding),
+            Activation(activations.relu),
+            BatchNormalization(axis = channelDim),
 
-    model.add(SeparableConv2D(128, (3,3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=channelDim))
-    
-    model.add(SeparableConv2D(128, (3,3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=channelDim))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+            SeparableConv2D(128, (3,3), padding = same_padding),
+            Activation(activations.relu),
+            BatchNormalization(axis = channelDim),
 
-    model.add(Flatten())
-    model.add(Dense(256))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
+            SeparableConv2D(128, (3,3), padding = same_padding),
+            Activation(activations.relu),
+            BatchNormalization(axis = channelDim),
+            MaxPooling2D(pool_size = (2,2)),
+            Dropout(0.25),
 
-    model.add(Dense(classes))
-    model.add(Activation("softmax"))
+            Flatten(),
+            Dense(256),
+            Activation(activations.relu),
+            BatchNormalization(),
+            Dropout(0.5),
 
-    return model
+            Dense(classes),
+            Activation(activations.softmax)
+        ])
+        
+        return model
